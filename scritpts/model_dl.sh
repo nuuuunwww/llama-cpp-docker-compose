379a4296cf6277c741b2b26221e07de021e25885
hiroshi kagawa <cs.0406.ikimon@gmail.com>
2025-08-30 16:02:24 +0900

    Comment out Gemma 3n E2B model download in script and retain mxbai model download

--- scritpts/model_dl.sh
+++ scritpts/model_dl.sh
@@ -1,6 +1,9 @@
-# Gemma 3n E2B モデルをダウンロード
-curl -L -o gemma3n-e2b-fixed.gguf \
-  https://huggingface.co/unsloth/gemma-3n-E2B-it-GGUF/resolve/main/gemma-3n-E2B-it-UD-Q4_K_XL.gguf
+# # Gemma 3n E2B モデルをダウンロード
+# curl -L -o gemma3n-e2b-fixed.gguf \
+#   https://huggingface.co/unsloth/gemma-3n-E2B-it-GGUF/resolve/main/gemma-3n-E2B-it-UD-Q4_K_XL.gguf
+
+curl -L -o mxbai-embed-large-v1.Q4_0.gguf \
+  https://huggingface.co/mxbai/mxbai-embed-large-v1/resolve/main/mxbai-embed-large-v1-f16.gguf
 
 # models/ ディレクトリに移動
 mv gemma3n-e2b-fixed.gguf models/
